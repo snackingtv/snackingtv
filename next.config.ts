d57e23e122204f9559a840b91ea13a5758aaaa57
Viktor Linger (Aplayersday) <aplayersday@gmail.com>
2025-11-07 07:36:24 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,16 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
