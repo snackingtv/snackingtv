2d186f980d8fd21c9203a52de5e268b6b3f2a030
Viktor Linger (Aplayersday) <aplayersday@gmail.com>
2025-11-07 05:48:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2607129172-1c6b4",
+  "appId": "1:83206065975:web:b999412c3506cb073714b2",
+  "apiKey": "AIzaSyAjJKKRziLeaTNwQjdu8Q3oSdLvbsw4YYM",
+  "authDomain": "studio-2607129172-1c6b4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "83206065975"
+};
