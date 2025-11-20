474d74e424ecb0ec6687601fdfd7740ca4982c1e
Viktor Linger (Aplayersday) <aplayersday@gmail.com>
2025-11-20 09:56:39 +0000

    Try fixing this error: `Runtime Error: Error: Failed to read the 'respon

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { cn } from '@/lib/utils';
 import Lottie from 'lottie-react';
 
