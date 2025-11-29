cea3c38c4d53683ea643f5b23208d3cf46c75b8a
Viktor Linger (Aplayersday) <aplayersday@gmail.com>
2025-11-29 12:44:21 +0000

    Überprüfe ob dieb Youtube Videos die neusten sind

--- src/app/lib/youtube-videos.ts
+++ src/app/lib/youtube-videos.ts
@@ -7,10 +7,16 @@ export interface YouTubeVideo {
 
 export const topYoutubeVideos: YouTubeVideo[] = [
   {
-    id: 'g_it_x3yHw8',
-    title: '$1 vs $1,000,000,000 House!',
-    thumbnailUrl: 'https://i.ytimg.com/vi/g_it_x3yHw8/maxresdefault.jpg',
-    videoUrl: 'https://www.youtube.com/watch?v=g_it_x3yHw8',
+    id: 'mP36tq-g45E',
+    title: 'I Built 100 Wells In Africa',
+    thumbnailUrl: 'https://i.ytimg.com/vi/mP36tq-g45E/maxresdefault.jpg',
+    videoUrl: 'https://www.youtube.com/watch?v=mP36tq-g45E',
+  },
+  {
+    id: 'zO2rA7t1a30',
+    title: 'I Survived A Plane Crash',
+    thumbnailUrl: 'https://i.ytimg.com/vi/zO2rA7t1a30/maxresdefault.jpg',
+    videoUrl: 'https://www.youtube.com/watch?v=zO2rA7t1a30',
   },
   {
     id: 'kXX61wN_E_M',
@@ -19,22 +25,16 @@ export const topYoutubeVideos: YouTubeVideo[] = [
     videoUrl: 'https://www.youtube.com/watch?v=kXX61wN_E_M',
   },
   {
-    id: '9bZkp7q19f0',
-    title: 'PSY - GANGNAM STYLE(강남스타일) M/V',
-    thumbnailUrl: 'https://i.ytimg.com/vi/9bZkp7q19f0/maxresdefault.jpg',
-    videoUrl: 'https://www.youtube.com/watch?v=9bZkp7q19f0',
+    id: 'g_it_x3yHw8',
+    title: '$1 vs $1,000,000,000 House!',
+    thumbnailUrl: 'https://i.ytimg.com/vi/g_it_x3yHw8/maxresdefault.jpg',
+    videoUrl: 'https://www.youtube.com/watch?v=g_it_x3yHw8',
   },
   {
-    id: 'L_jWHffIx5E',
-    title: 'Sing 2 - I Still Haven’t Found What I’m Looking For',
-    thumbnailUrl: 'https://i.ytimg.com/vi/L_jWHffIx5E/maxresdefault.jpg',
-    videoUrl: 'https://www.youtube.com/watch?v=L_jWHffIx5E',
-  },
-  {
-    id: 'u9prcUCHlqM',
-    title: 'The Weeknd - Blinding Lights (Official Video)',
-    thumbnailUrl: 'https://i.ytimg.com/vi/u9prcUCHlqM/maxresdefault.jpg',
-    videoUrl: 'https://www.youtube.com/watch?v=u9prcUCHlqM'
+    id: 'YlI5C-42_m8',
+    title: 'Automower® NERA (German)',
+    thumbnailUrl: 'https://i.ytimg.com/vi/YlI5C-42_m8/maxresdefault.jpg',
+    videoUrl: 'https://www.youtube.com/watch?v=YlI5C-42_m8'
   },
   {
     id: 'fLexgOvhW3U',
@@ -49,23 +49,21 @@ export const topYoutubeVideos: YouTubeVideo[] = [
     videoUrl: 'https://www.youtube.com/watch?v=2Vv-BfVoq4g',
   },
   {
-    id: 'Rbm6GXllBiw',
-    title: 'Maroon 5 - Girls Like You ft. Cardi B (Official Music Video)',
-    thumbnailUrl: 'https://i.ytimg.com/vi/Rbm6GXllBiw/maxresdefault.jpg',
-    videoUrl: 'https://www.youtube.com/watch?v=Rbm6GXllBiw',
+    id: 'u9prcUCHlqM',
+    title: 'The Weeknd - Blinding Lights (Official Video)',
+    thumbnailUrl: 'https://i.ytimg.com/vi/u9prcUCHlqM/maxresdefault.jpg',
+    videoUrl: 'https://www.youtube.com/watch?v=u9prcUCHlqM'
   },
   {
-    id: 'hT_nvWreIhg',
-    title: 'Shakira - Waka Waka (This Time for Africa)',
-    thumbnailUrl: 'https://i.ytimg.com/vi/hT_nvWreIhg/maxresdefault.jpg',
-    videoUrl: 'https://www.youtube.com/watch?v=hT_nvWreIhg',
+    id: 'L_jWHffIx5E',
+    title: 'Sing 2 - I Still Haven’t Found What I’m Looking For',
+    thumbnailUrl: 'https://i.ytimg.com/vi/L_jWHffIx5E/maxresdefault.jpg',
+    videoUrl: 'https://www.youtube.com/watch?v=L_jWHffIx5E',
   },
   {
-    id: 'zO2rA7t1a30',
-    title: 'I Survived A Plane Crash',
-    thumbnailUrl: 'https://i.ytimg.com/vi/zO2rA7t1a30/maxresdefault.jpg',
-    videoUrl: 'https://www.youtube.com/watch?v=zO2rA7t1a30',
+    id: '9bZkp7q19f0',
+    title: 'PSY - GANGNAM STYLE(강남스타일) M/V',
+    thumbnailUrl: 'https://i.ytimg.com/vi/9bZkp7q19f0/maxresdefault.jpg',
+    videoUrl: 'https://www.youtube.com/watch?v=9bZkp7q19f0',
   },
 ];
-
-    
